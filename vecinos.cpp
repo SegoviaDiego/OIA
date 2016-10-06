9f8155cb0d48339d81c9efc23989e4dbd40a10b3
Holthain <segoviadiegomatias@gmail.com>
2016-10-06 17:11:01 -0300

    Nivel 2 Problema 3 VECINOS Certamen Nacional OIA 2006

---
+++ vecinos.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 10005
+#define INF -1
+
+struct cmp{
+    bool operator()(const pair<int,int> a,const pair<int,int> b){
+        return a.second < b.second;
+    }
+};
+
+int distancia1[MAX];
+bool visitado1[MAX];
+int distancia2[MAX];
+bool visitado2[MAX];
+priority_queue < pair<int,int>, vector< pair<int,int> >, cmp> Q;
+
+void relajacion1(const int actual, const int adyacente, const int peso){
+    if(distancia1[actual] + peso > distancia1[adyacente]){
+        distancia1[adyacente]=distancia1[actual] + peso;
+        Q.push(make_pair(adyacente,distancia1[adyacente]));
+    }
+}
+
+void relajacion2(const int actual, const int adyacente, const int peso){
+    if(distancia2[actual] + peso > distancia2[adyacente]){
+        distancia2[adyacente]=distancia2[actual] + peso;
+        Q.push(make_pair(adyacente,distancia2[adyacente]));
+    }
+}
+
+int main(){
+    ifstream fin("in.txt");
+
+    int vecinos,lazos,x,y,in1,in2,in3,actual,x1=0,y1=0;
+
+    fin >> vecinos >> lazos >> x >> y;
+
+    vector< vector< pair<int,int> > > lista(vecinos+1);
+
+    for(int i=1;i<vecinos+1;i++){
+        distancia1[i]=INF;
+        distancia2[i]=INF;
+        visitado1[i]=false;
+        visitado2[i]=false;
+    }
+
+    while(fin >> in1 >> in2 >> in3){
+        lista[in1].push_back(make_pair(in2,in3));
+        lista[in2].push_back(make_pair(in1,in3));
+    }
+
+    //Inicio dijkstra para el PRIMER vecino
+
+    Q.push(make_pair(x,0));
+    distancia1[x]=0;
+    while(!Q.empty()){
+        actual = Q.top().first;
+        Q.pop();
+
+        if(visitado1[actual]) continue;
+        visitado1[actual]=true;
+
+        for(int i=0;i<lista[actual].size();i++){
+            if(!visitado1[ lista[actual][i].first ]){
+                relajacion1(actual,lista[actual][i].first,lista[actual][i].second);
+            }
+        }
+    }
+
+    //Inicio dijkstra para el SEGUNDO vecino
+
+    Q.push(make_pair(y,0));
+    distancia2[y]=0;
+    while(!Q.empty()){
+        actual = Q.top().first;
+        Q.pop();
+
+        if(visitado2[actual]) continue;
+        visitado2[actual]=true;
+
+        for(int i=0;i<lista[actual].size();i++){
+            if(!visitado2[ lista[actual][i].first ]){
+                relajacion2(actual,lista[actual][i].first,lista[actual][i].second);
+            }
+        }
+    }
+
+    //Comprobaciones
+
+    for(int i=1;i<vecinos+1;i++){
+        if(i!=x && i!=y && distancia1[i] != distancia2[i]){
+            if(distancia1[i] > distancia2[i]){
+                x1++;
+            } else {
+                y1++;
+            }
+        }
+    }
+    cout << x1 << ' ' << y1 << endl;
+}
