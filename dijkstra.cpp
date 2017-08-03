4ca9d1710f954976940c4ebf1920a3eecd55c5df
Holthain <segoviadiegomatias@gmail.com>
2017-08-02 21:31:24 -0300

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,4 +1,4 @@
-//Input
+//TIMING: 5:32:12
 5 1 5
 1 2 7
 1 4 2
@@ -10,78 +10,70 @@
 4 5 5
 5 3 4
 #include <bits/stdc++.h>
+#define MAX 10000
+#define INF 1<<30
 using namespace std;
-
 struct cmp{
-    bool operator()(const pair<int,int> a, const pair<int,int> b){
+    operator()(pair<int,int> a,pair<int,int> b){
         return a.second > b.second;
     }
 };
-
-#define MAX 5000
-#define INF 1<<30
-
-int n,m,o,d;
-int previo[MAX];
-int distancia[MAX];
 bool visitado[MAX];
+int distancia[MAX];
+int previo[MAX];
 vector< vector< pair<int,int> > > lista(MAX);
-priority_queue< pair<int,int>, vector< pair<int,int> >, cmp > Q;
+priority_queue< pair<int,int>, vector< pair<int,int> >, cmp> Q;
+int n,o,d,a,b,c;
 
-void relajacion(const int act,const int ady,const int peso){
-    if(distancia[act] + peso < distancia[ady]){
-        distancia[ady]=distancia[act] + peso;
-        previo[ady]=act;
-        Q.push(make_pair(ady,distancia[ady]));
-    }
-}
-
-void init(){
-    for(int i=1;i<n+1;i++){
-        visitado[i]=false;
-        distancia[i]=INF;
-        previo[i]=i;
+void relax(int act, int ady, int peso){
+    if(peso + distancia[act] < distancia[ady]){
+        distancia[ady] = distancia[act] + peso;
+        previo[ady] = act;
+        Q.push(make_pair(ady, distancia[ady]));
     }
 }
 
 void dijkstra(){
-    int act,ady,peso;
-    Q.push(make_pair(o,0));
     distancia[o]=0;
-
+    Q.push(make_pair(o,0));
+    int act,ady,peso;
     while(!Q.empty()){
         act = Q.top().first;
         Q.pop();
 
         if(visitado[act]) continue;
-        visitado[act]=true;
+        visitado[act] = true;
 
         for(int i=0;i<lista[act].size();i++){
-            ady=lista[act][i].first;
-            peso=lista[act][i].second;
-            if(!visitado[ady]){
-                relajacion(act,ady,peso);
-            }
+            ady = lista[act][i].first;
+         peso= lista[act][i].second;
+            if(!visitado[ady]) relax(act,ady,peso);
         }
     }
 }
 
-void escribir(const int a){
+void escribir(int a) {
     if(previo[a] != a)
         escribir(previo[a]);
     cout << a << ' ';
+}
+
+void init(){
+    for(int i=1;i<=n;i++){
+        visitado[i] = false;
+        distancia[i] = INF;
+        previo[i] = i;
+    }
 }
 
 int main(){
     ifstream fin("in.txt");
     fin >> n >> o >> d;
 
-    int a,b,c;
     init();
 
-    while(fin >> a >> b >> c){
+    while(fin >> a >> b >> c)
         lista[a].push_back(make_pair(b,c));
-    }
 
     dijkstra();
 
